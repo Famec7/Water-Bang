e4cecd5a35d2e3d0bd28335d4e90ddbdb929b6b7
Mono <wz9384@gmail.com>
2023-07-13 14:06:03 +0900

    캐릭터 오브젝트 스크립트

---
+++ Water Bang/Assets/Script/Character.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public void CharacterMove()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
